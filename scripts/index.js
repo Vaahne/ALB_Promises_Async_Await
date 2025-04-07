fda1e38a4c66cac9f071682800ebfe51fd957b45
Spring -Git <vaahne@yahoo.com>
2025-04-06 23:31:33 -0600

    formating

--- scripts/index.js
+++ scripts/index.js
@@ -2,59 +2,56 @@
 import { central, db1, db2, db3, vault } from "./databases.js";
 
 function getUserData(id) {
-  const dbs = {
-    db1: db1,
-    db2: db2,
-    db3: db3
-  };
+    const dbs = {
+      db1: db1,
+      db2: db2,
+      db3: db3
+    };
 
-//  using Async/Await
-  async function getData(){
+    //  using Async/Await
+    async function getData() {
+      let getDB = await central(id);
+      let data1 = await dbs[getDB](id);
+      let data2 = await vault(id);
+      let data = Object.assign({}, data1, data2);
+      return data;
+    }
 
-    let getDB = await central(id);
-    let data1 = await dbs[getDB](id);
-    let data2 = await vault(id);
-    let data = Object.assign({},data1,data2);
+    // using promises
+    function usingPromises() {
+      return central(id)
+        .then((getDB) => {
+          return dbs[getDB](id)
+        })
+        .then((data1) => {
+          return vault(id).then((data2) => {
+            let data;
+            data = Object.assign({}, data1, data2);
+            return data;
+          })
+        })
+        .catch((err) => {
+          console.error(err.message)
+        })
+    }
+    return usingPromises();   // Data using promises
 
-    return data;
-  }
-
-// using promises
-  function usingPromises(){
-    return central(id)
-    .then((getDB)=>{
-      return dbs[getDB](id)
-    })
-    .then((data1)=>{
-      return vault(id).then((data2)=>{
-          let data;
-          data = Object.assign({},data1,data2);
-          return data;
-      })
-    })
-    .catch((err)=>{
-      console.error(err.message)
-    })    
-  }
-
-  return usingPromises();   // Data using promises
-
-// comment above line and uncomment below line to use async/await approach
-//   return getData();      // Data using async/await
-
+    // comment above line and uncomment below line to use async/await approach
+    //   return getData();      // Data using async/await
 }
 
-// data from db1 
- getUserData(2).then((data) => console.log(data));
+
+// data from db1  Single promise
+getUserData(2).then((data) => console.log(data));
 
 
 // All data from 3 different dbs will be displayed in an array.
-Promise.all([getUserData(3),getUserData(7),getUserData(9)])
-.then((msg)=>{
-  console.log(msg)
-})
+Promise.all([getUserData(3), getUserData(7), getUserData(9)])
+  .then((msg) => {
+    console.log(msg)
+  })
 
 // data from Allsettled 
-Promise.allSettled([getUserData(1),getUserData(6)]).then((msg)=>{
+Promise.allSettled([getUserData(1), getUserData(6)]).then((msg) => {
   console.log(msg)
 })
